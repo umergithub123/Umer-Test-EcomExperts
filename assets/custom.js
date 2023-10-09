adeedd8f846079bf85c6cb38883b781bb7122a7c
Umer <umerchaudhry.089@gmail.com>
2023-10-09 14:59:15 +0500

    sedning js

--- assets/custom.js
+++ assets/custom.js
@@ -2,12 +2,19 @@
 let inputs = document.querySelectorAll('input[type=radio][name=Color]');
 let color_dropdown = document.querySelector('select[name="options[Color]"]');
 let size_dropdown = document.querySelector('select[name="options[Size]"]');
+let all_imgs = document.querySelectorAll('.product__media img');
+
 
 inputs.forEach((input)=>{
     input.addEventListener('click',(e)=>{
       let currnt_input = e.target;
       let value = currnt_input.getAttribute('value');
       color_dropdown.value = value;
+      let currnt_img = document.querySelector('.product__media-item img[alt="'+currnt_value+'"]');
+      all_imgs.forEach((img)=>{
+      img.closest('.product__media-item').style.display =  'none';               
+      })        
+      currnt_img.closest('.product__media-item').style.display = 'block';
     })
 })
 
